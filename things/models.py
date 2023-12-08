7d52bb8331a9fa6c2777ddceae1d3d92fd43e041
Deyu Li <k21081622@kcl.ac.uk>
2023-12-08 09:11:41 +0000

    correct  exercise 1 and 2

--- things/models.py
+++ things/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class thing(models.Model):
+class Thing(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=520)
     quantity = models.IntegerField()
